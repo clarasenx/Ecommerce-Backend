281cc76dfaabeb7f5657bb525d20bedda5c99503
Clara Helena <clarahelenasena@gmail.com>
2024-08-08 12:46:59 -0400

    add a router to get user by id

--- repository/BaseRepository.js
+++ repository/BaseRepository.js
@@ -1,10 +1,24 @@
-import pool from './db.js'
+import pool from "./db.js";
 
 class BaseRepository {
   async getAll(table) {
-    const results = (await pool.query(`SELECT * FROM ${table}`)).rows;
-    return results;
+    try {
+      const results = (await pool.query(`SELECT * FROM ${table}`)).rows;
+      return results;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getById(table, id) {
+    try {
+      const queryText = `SELECT * FROM ${table} WHERE id = $1`;
+      const result = (await pool.query(queryText, [id])).rows[0];
+      return result;
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
-export default BaseRepository
+export default BaseRepository;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,4 +8,10 @@ router.get("/", async (req, res) => {
   res.status(200).send(result);
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const result = await new BaseRepository().getById("users", id);
+  res.status(200).send(result);
+})
+
 export default router;
